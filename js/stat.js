1ba7eb5ad7b23cffc9b04ecfd56c55c662e1ad43
DangerPNZ <dangerpnz@mail.ru>
2017-01-25 23:40:13 +0400

    Правка: убрал две лишних функции

--- js/stat.js
+++ js/stat.js
@@ -33,19 +33,11 @@ window.renderStatistics = function (ctx, names, times) {
   // Интервал между колонками
   var columnIndent = 90;
   function colorGenerate() {
-    ctx.fillStyle = ['rgba(0, 0,', ((Math.random() * 5) * 50).toFixed(0), ',', (Math.random()).toFixed(1), ')'].join('');
+    ctx.fillStyle = 'rgba(0, 0, ' + (Math.random() * 255).toFixed(0) + ', ' + (Math.random() * (1.5 - 0.5) + 0.5).toFixed(1) + ')';
   }
   function histoDraw(bottomLine, columnWidth) {
     // Отрисовка гистограммы (начальная точка по оси Х(x1) + интервал между колонками * индекс, Начальная точка У, ширина, (??? высота???))
     ctx.fillRect(histoX + columnIndent * i, (histoX - height + bottomLine), columnWidth, height);
-  }
-  function playerNameOutput(color, y) {
-    ctx.strokeStyle = color;
-    ctx.strokeText(name + ':', histoX + columnIndent * i, histoHeight + y);
-  }
-  function playerResultsOutput(color, y) {
-    ctx.fillStyle = color;
-    ctx.fillText(time.toFixed(0), histoX + columnIndent * i, (histoX - height + y));
   }
   for (i = 0; i < times.length; i++) {
     time = times[i];
@@ -57,7 +49,7 @@ window.renderStatistics = function (ctx, names, times) {
       colorGenerate();
     }
     histoDraw(100, 40);
-    playerNameOutput('black', 113);
-    playerResultsOutput('black', 80);
+    strokeTextWrite(name + ':', 'black', histoX + columnIndent * i + 0, histoHeight + 113);
+    fillTextWrite(time.toFixed(0), 'black', histoX + columnIndent * i + 0, histoX - height + 80);
   }
 };
