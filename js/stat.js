f6173da7c03931c13b18895bc3d837d6619428a1
DangerPNZ <dangerpnz@mail.ru>
2017-01-25 20:36:07 +0400

    Дополнение по функциям

--- js/stat.js
+++ js/stat.js
@@ -7,8 +7,16 @@ window.renderStatistics = function (ctx, names, times) {
   ctx.font = '16px PT Mono';
   ctx.textBaseline = 'hanging';
   ctx.fillStyle = 'rgb(0, 0, 0)';
-  ctx.strokeText('Ура, Вы победили!', 230, 25);
-  ctx.fillText('Список результатов:', 222, 45);
+  function strokeTextWrite(text, color, x, y) {
+    ctx.strokeStyle = color;
+    ctx.strokeText(text, x, y);
+  }
+  function fillTextWrite(text, color, x, y) {
+    ctx.fillStyle = color;
+    ctx.fillText(text, x, y);
+  }
+  strokeTextWrite('Ура, Вы победили!', 'darkblue', 230, 25);
+  fillTextWrite('Список результатов:', '#000', 222, 45);
   var max = times[0];
   for (var i = 0; i < times.length; i++) {
     var time = times[i];
@@ -24,6 +32,21 @@ window.renderStatistics = function (ctx, names, times) {
   var step = histoHeight / max;
   // Интервал между колонками
   var columnIndent = 90;
+  function colorGenerate() {
+    ctx.fillStyle = ['rgba(0, 0,', ((Math.random() * 5) * 50).toFixed(0), ',', (Math.random()).toFixed(1), ')'].join('');
+  }
+  function histoDraw(bottomLine, columnWidth) {
+    // Отрисовка гистограммы (начальная точка по оси Х(x1) + интервал между колонками * индекс, Начальная точка У, ширина, (??? высота???))
+    ctx.fillRect(histoX + columnIndent * i, (histoX - height + bottomLine), columnWidth, height);
+  }
+  function playerNameOutput(color, y) {
+    ctx.strokeStyle = color;
+    ctx.strokeText(name + ':', histoX + columnIndent * i, histoHeight + y);
+  }
+  function playerResultsOutput(color, y) {
+    ctx.fillStyle = color;
+    ctx.fillText(time.toFixed(0), histoX + columnIndent * i, (histoX - height + y));
+  }
   for (i = 0; i < times.length; i++) {
     time = times[i];
     var name = names[i];
@@ -31,12 +54,10 @@ window.renderStatistics = function (ctx, names, times) {
     if (name === 'Вы') {
       ctx.fillStyle = 'rgba(255, 0, 0, 1)';
     } else {
-      ctx.fillStyle = ['rgba(0, 0,', ((Math.random() * 5) * 50).toFixed(0), ',', (Math.random()).toFixed(1), ')'].join('');
+      colorGenerate();
     }
-    // Отрисовка гистограммы (начальная точка по оси Х(x1) + интервал между колонками * индекс, Начальная точка У, ширина, (??? высота???))
-    ctx.fillRect(histoX + columnIndent * i, (histoX - height + 100), 40, height);
-    ctx.fillStyle = '#000';
-    ctx.strokeText(name + ':', histoX + columnIndent * i, histoHeight + 113);
-    ctx.fillText(time.toFixed(0), histoX + columnIndent * i, (histoX - height + 80));
+    histoDraw(100, 40);
+    playerNameOutput('black', 113);
+    playerResultsOutput('black', 80);
   }
 };
